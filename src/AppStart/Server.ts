dba4f41602acc9ccb9d025fa409d6a12708ed80f
Sébastien Tachier <me@sebundefined.fr>
2018-12-12 21:32:52 +0100

    Adding init app

---
+++ src/AppStart/Server.ts
@@ -0,0 +1,79 @@
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
+import * as logger from "morgan";
+import * as path from "path";
+import errorHandler = require("errorhandler");
+import methodOverride = require("method-override");
+
+/**
+ * The server.
+ *
+ * @class Server
+ */
+export default class Server {
+
+  public app: express.Application;
+
+  /**
+   * Bootstrap the application.
+   *
+   * @class Server
+   * @method bootstrap
+   * @static
+   * @return {ng.auto.IInjectorService} Returns the newly created injector for this app.
+   */
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  /**
+   * Constructor.
+   *
+   * @class Server
+   * @constructor
+   */
+  constructor() {
+    //create expressjs application
+    this.app = express();
+
+    //configure application
+    this.config();
+
+    //add routes
+    this.routes();
+
+    //add api
+    this.api();
+  }
+
+  /**
+   * Create REST API routes
+   *
+   * @class Server
+   * @method api
+   */
+  public api() {
+    //empty for now
+  }
+
+  /**
+   * Configure application
+   *
+   * @class Server
+   * @method config
+   */
+  public config() {
+    //empty for now
+  }
+
+  /**
+   * Create router
+   *
+   * @class Server
+   * @method api
+   */
+  public routes() {
+    //empty for now
+  }
+}
---
+++ src/InitApp.ts
@@ -0,0 +1,58 @@
+import * as Server from "./AppStart/Server"
+import * as Debug from "debug"
+import * as Http from 'http'
+
+
+class InitApp {
+
+    private httpPort: number
+    private app: any
+    private httpServer:Http.Server
+
+    constructor() {
+        this.httpPort = this.normalizePort(process.env.PORT || 8080);
+        this.app = Server.default.bootstrap().app
+        this.app.set("port", this.httpPort)
+        this.httpServer = Http.createServer(this.app)
+    }
+
+    public static initialize() {
+        let myApp = new InitApp()
+        myApp.httpServer.listen(myApp.httpPort);
+
+        myApp.httpServer.on("listening", function(){
+            const debug = Debug("kkj");
+            debug("pouet")
+            let url = myApp.httpServer.address()
+            let bind = typeof url === "string"
+                ? "pipe " + url
+                : "port " + url.port;
+            debug("Listening on " + bind);
+        });
+    }
+    /**
+     * Normalize port
+     * @param val 
+     */
+    private normalizePort(val) {
+        var port = parseInt(val, 10);
+      
+        if (isNaN(port)) {
+          // named pipe
+          return val;
+        }
+      
+        if (port >= 0) {
+          // port number
+          return port;
+        }
+      
+        return false;
+      }
+      
+      
+}
+
+let myApp = new InitApp()
+
+InitApp.initialize()
